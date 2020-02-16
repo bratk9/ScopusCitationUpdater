7fcbfc97b98365e583a32ae536568310c52614db
HPEONIX <36728707+HPEONIX@users.noreply.github.com>
2020-02-16 19:15:55 +0530

    added apis

--- fileuplodev-1.py
+++ fileuplodev-1.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, make_response
+import requests
 import csv
 import os
 import weasyprint
+import json
 
 app = Flask(__name__)
 
@@ -15,24 +17,39 @@ def getpostmet():
         return render_template('upload.html')
     elif request.method == 'POST':
         file = request.files['file']
-        file.stream.seek(0)
-        myfile=file.read().decode("utf-8")
-        data=csv.reader(myfile.split('\n'),delimiter=',')
-        generated=[]
-        for info in data:
-            #code for getting citation  :  todo
-            generated.append(info)
-            #append profassor info and citation to info
-        pdf_outline=render_template('pdf_template.html',result=generated)
-        # pdf=pdfkit.from_string(pdf_outline,False)
+        if file:
+            file.stream.seek(0)
+            myfile=file.read().decode("utf-8")
+            data=csv.reader(myfile.split('\n'),delimiter=',')
+            generated=[]
+            for info in data:
+                #code for getting citation  :  todo
 
-        pdf=weasyprint.HTML(string=pdf_outline).write_pdf()
+                if(info and len(info)!=0 and info[1] and len(info[1])!=0 and info[0] and len(info[0])!=0):
+                    res=requests.get("http://api.elsevier.com/content/author",
+                                        params={"orcid":info[1]},
+                                        headers={'Accept':'application/json',
+                                        'X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
+                    respdat=dict(res.json())
+                    if list(respdat)[0]=="service-error":
+                        print(info[0],"error")
+                    else:
+                        print(info[0],respdat['author-retrieval-response'][0]['coredata']['citation-count'])
+                        generated.append([info[0],respdat['author-retrieval-response'][0]['coredata']['citation-count']])
 
-        resp=make_response(pdf)
-        resp.headers['Content-Type']="application/pdf"
-        resp.headers['Content-Disposition']="inline;filename=Citations.pdf"
-
-        return resp
+                #generated.append(info)
+                #append profassor info and citation to genetated
+            pdf_outline=render_template('pdf_template.html',result=generated)
+    
+            pdf=weasyprint.HTML(string=pdf_outline).write_pdf()
+    
+            resp=make_response(pdf)
+            resp.headers['Content-Type']="application/pdf"
+            resp.headers['Content-Disposition']="inline;filename=Citations.pdf"
+    
+            return resp
+        else:
+            return render_template('upload.html')
         
 if __name__ == '__main__':
    app.run(debug = True)
