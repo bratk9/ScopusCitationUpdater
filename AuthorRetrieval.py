2e47b87343185f79a8501b85012eb0586ab18efa
HPEONIX <36728707+HPEONIX@users.noreply.github.com>
2020-03-15 21:58:34 +0530

    try cached

--- AuthorRetrieval.py
+++ AuthorRetrieval.py
@@ -11,29 +11,32 @@ print("1. ORCID")
 print("2. Author ID")
 print("3. EID")
 c = int(input("Enter choice: "))
-if c==1:
-    n = input("Enter ORCID: ")
-    url = "http://api.elsevier.com/content/author?orcid=" + n + "&view=metrics"
-    resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
-elif c==2:
-    n = input("Enter Author ID: ")
-    url = "http://api.elsevier.com/content/author?author_id=" + n + "&view=metrics"
-    resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
-elif c==3:
-    n = input("Enter EID ID: ")
-    url = "http://api.elsevier.com/content/author?eid=" + n + "&view=metrics"
-    resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
-else:
-    print("Invalid choice!")
-    quit(keep_kernel=True)
+try:
+    if c==1:
+        n = input("Enter ORCID: ")
+        url = "http://api.elsevier.com/content/author?orcid=" + n + "&view=metrics"
+        resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
+    elif c==2:
+        n = input("Enter Author ID: ")
+        url = "http://api.elsevier.com/content/author?author_id=" + n + "&view=metrics"
+        resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
+    elif c==3:
+        n = input("Enter EID ID: ")
+        url = "http://api.elsevier.com/content/author?eid=" + n + "&view=metrics"
+        resp = requests.get(url, headers={'Accept':'application/json','X-ELS-APIKey': '24783270e58eb56ff94c059e8c7eb44c'})
+    else:
+        print("Invalid choice!")
+        quit(keep_kernel=True)
 
-#Remove &view=metrics to get all information
+    #Remove &view=metrics to get all information
 
-out = (json.dumps(resp.json(),
-                 sort_keys=True,
-                 indent=4, separators=(',', ': ')))
-out1 = dict(resp.json())
-if list(out1)[0]=="service-error":
-    print("SERVICE ERROR!")
-else:
-    print("CITATION COUNT:" + str(out1['author-retrieval-response'][0]['coredata']['citation-count']))
+    out = (json.dumps(resp.json(),
+                     sort_keys=True,
+                     indent=4, separators=(',', ': ')))
+    out1 = dict(resp.json())
+    if list(out1)[0]=="service-error":
+        print("SERVICE ERROR!")
+    else:
+        print("CITATION COUNT:" + str(out1['author-retrieval-response'][0]['coredata']['citation-count']))
+except:
+    print("Connect to a valid network")
